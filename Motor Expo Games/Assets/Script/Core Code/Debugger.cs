1d9427bcecc63ce032647c21f991bff4c0c38003
twotthep <twotthep@me.com>
2020-08-10 09:30:05 +0700

    fixed exception

--- Motor Expo Games/Assets/Script/Core Code/Debugger.cs
+++ Motor Expo Games/Assets/Script/Core Code/Debugger.cs
@@ -16,11 +16,15 @@ public class Debugger : MonoBehaviour
     }
     void Update()
     {
-        textincanvas.GetComponent<UnityEngine.UI.Text>().text = "power : " + test.array1[test.selected1].power + test.array2[test.selected2].power + test.array3[test.selected3].power + "\n" + 
+        if(textincanvas != null)
+        {
+            textincanvas.GetComponent<UnityEngine.UI.Text>().text = "power : " + test.array1[test.selected1].power + test.array2[test.selected2].power + test.array3[test.selected3].power + "\n" +
                                                                 "weight : " + test.array1[test.selected1].weight + test.array2[test.selected2].weight + test.array3[test.selected3].weight + "\n" +
                                                                 "movement : " + test.array1[test.selected1].movementspeed + test.array2[test.selected2].movementspeed + test.array3[test.selected3].movementspeed + "\n" +
                                                                 "strength : " + test.array1[test.selected1].strength + test.array2[test.selected2].strength + test.array3[test.selected3].strength
                                                                 ;
+        }
+        
 
         /*/
         boatstatus.Player1.weight = array1[selected1].weight + array2[selected2].weight + array3[selected3].weight;
