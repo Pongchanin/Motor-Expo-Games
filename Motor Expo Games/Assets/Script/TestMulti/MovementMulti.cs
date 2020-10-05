7a6c0ec9d5879d2d0d2684ad3caef5b06c2e6dd8
twotthep <twotthep@me.com>
2020-10-05 15:49:42 +0700

    WIP

--- Motor Expo Games/Assets/Script/TestMulti/MovementMulti.cs
+++ Motor Expo Games/Assets/Script/TestMulti/MovementMulti.cs
@@ -6,11 +6,13 @@ using UnityEngine.Networking;
 public class MovementMulti : NetworkBehaviour
 {
     Joystick joystick;
+    GameObject Boat;
     // Start is called before the first frame update
     public int moveSpeed;
     void Start()
     {
         joystick = FindObjectOfType<Joystick>();
+        Boat = GetComponent<MovementMulti>().gameObject;
     }
     public override void OnStartLocalPlayer()
     {
@@ -18,29 +20,47 @@ public class MovementMulti : NetworkBehaviour
     }
 
     // Update is called once per frame
-    [ServerCallback][ClientCallback]
     void Update()
     {
-        if (isLocalPlayer && isClient)
-        {
-            if ((joystick.input.x > 0 || joystick.input.x < 0))
-            {
-                transform.Translate(new Vector3(joystick.input.x * (moveSpeed) * Time.deltaTime, 0f, 0f));
 
-                /*angle = Mathf.Atan2(joystick.input.y, joystick.input.x) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);*/
-                //print("X: " + joystick.input.x);
-                // transform.rotation = new Quaternion(0, 0, 180 * joystick.input.x,0);
-            }
-            if ((joystick.input.y > 0 || joystick.input.y < 0))
-            {
-                transform.Translate(new Vector3(0f, joystick.input.y * (moveSpeed) * Time.deltaTime, 0f));
-
-                /*angle = Mathf.Atan2(joystick.input.y, joystick.input.x) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);*/
-                //print("Y: " + joystick.input.y);
-            }
-        }
+        Move();
         
+    }
+
+    void Move()
+    {
+
+        if (joystick.input.x > 0 || joystick.input.x < 0)
+        {
+            
+            Boat.transform.position = new Vector3(joystick.input.x * (moveSpeed) * Time.deltaTime, 0f, 0f);
+            CmdProvidePositiontoServer(Boat, new Vector3(joystick.input.x * (moveSpeed) * Time.deltaTime, 0f, 0f));
+            /*angle = Mathf.Atan2(joystick.input.y, joystick.input.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);*/
+            //print("X: " + joystick.input.x);
+            // transform.rotation = new Quaternion(0, 0, 180 * joystick.input.x,0);
+        }
+        if (joystick.input.y > 0 || joystick.input.y < 0)
+        {
+            transform.Translate(new Vector3(0f, joystick.input.y * (moveSpeed) * Time.deltaTime, 0f));
+            CmdProvidePositiontoServer(Boat, new Vector3(0f, joystick.input.y * (moveSpeed) * Time.deltaTime, 0f));
+            /*angle = Mathf.Atan2(joystick.input.y, joystick.input.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);*/
+            //print("Y: " + joystick.input.y);
+        }
+
+    }
+
+    [Command]
+    void CmdProvidePositiontoServer(GameObject BBoat, Vector3 pos)
+    {
+        RpcTransmitplayerTransform(BBoat, pos);
+
+    }
+
+    [ClientRpc][ClientCallback]
+    void RpcTransmitplayerTransform(GameObject BBoat, Vector3 pos)
+    {
+        BBoat.transform.position = pos;
     }
 }
