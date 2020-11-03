92099df1bc59dcb1b0755f5f653a878c23c6b281
twotthep <twotthep@me.com>
2020-11-03 12:10:35 +0700

    Auto Discovered IP

--- Motor Expo Games/Assets/NetworkLobbyButton.cs
+++ Motor Expo Games/Assets/NetworkLobbyButton.cs
@@ -2,31 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
-
+using Mirror.Discovery;
 [System.Obsolete]
 public class NetworkLobbyButton : NetworkBehaviour
 {
     NetworkRoomManager networkLobby;
+    NetworkDiscovery networkDiscovery;
+    NetworkManager networkManager;
+
+    Dictionary<long, ServerResponse> discoveredServers = new Dictionary<long, ServerResponse>();
     public bool isPressed;
     // Start is called before the first frame update
     void Start()
     {
         networkLobby = GameObject.FindObjectOfType<NetworkRoomManager>();
+        networkDiscovery = FindObjectOfType<NetworkDiscovery>();
+        networkManager = FindObjectOfType<NetworkManager>();
+        InvokeRepeating("Discovery", 0, 1.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //networkDiscovery.StartDiscovery();
     }
     public void Host()
     {
-        networkLobby.StartHost();
+        networkManager.StartHost();
+        networkDiscovery.AdvertiseServer();
         isPressed = true;
     }
     public void Join()
     {
-        networkLobby.StartClient();
-        isPressed = true;
+        if(discoveredServers.Count == 0)
+        {
+            networkManager.StartClient(discoveredServers[0].uri);
+            networkDiscovery.AdvertiseServer();
+            isPressed = true;
+        }
+       // networkLobby.StartClient();
+        
+        
+    }
+    void Discovery()
+    {
+        networkDiscovery.StartDiscovery();
+        
+        
     }
 }
